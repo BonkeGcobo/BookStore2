fd23c49efacbf072ef21f3d543174774af073b56
BonkeG <bonkegcobo291195@gmail.com>
2022-03-25 07:24:32 +0200

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import BookPage from './pages/BookPage';
+import Header from './components/Header';
+import Category from './components/Category/Category';
 
 const App = () => (
-  <div>
-    <BookPage />
-  </div>
+  <>
+    <Header />
+    <Routes>
+      <Route path="/" element={<BookPage />} />
+      <Route path="Categories" element={<Category />} />
+    </Routes>
+  </>
 );
 
 export default App;
---
+++ src/components/Category/Category.js
@@ -0,0 +1,9 @@
+function Category() {
+  return (
+    <div>
+      <h1>Coming Soon</h1>
+    </div>
+  );
+}
+
+export default Category;
