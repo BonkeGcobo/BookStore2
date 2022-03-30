b9fda4d1ef8178b0f5f453254ffdf38fb51dda7b
BonkeG <bonkegcobo291195@gmail.com>
2022-03-30 17:40:28 +0200

    Add redux for category

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,4 +1,9 @@
+import { useDispatch } from 'react-redux';
+import { checkStatus } from '../../redux/categories/categories';
+
 function Category() {
+  const dispatch = useDispatch();
+  dispatch(checkStatus());
   return (
     <div>
       <button type="submit">Check Status</button>
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { logger } from 'redux-logger';
 import booksReducer from './books/books';
+import categoryReducer from './categories/categories';
 
 const reducer = combineReducers({
   booksReducer,
+  categoryReducer,
   // Additional Reducers will be added here
 });
 
