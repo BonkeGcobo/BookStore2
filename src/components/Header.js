c3b8ccc490cc37c36e7cff2f50b8ed74c8d795cb
BonkeG <bonkegcobo291195@gmail.com>
2022-04-01 18:08:41 +0200

    Add classes on the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
   const links = [
@@ -18,15 +20,20 @@ const Header = () => {
   return (
     <header className="theHeader">
       <div className="HeaderRight">
-        <h1 className="title">Bookstore CMS</h1>
+        <h1 className="Apptitle">Bookstore CMS</h1>
         <ul className="nav">
           {links.map((link) => (
             <li key={link.id} className="link">
-              <NavLink to={link.path} activeClassName="active-link" exact>{link.text}</NavLink>
+              <NavLink to={link.path} activeClassName="active-link" className="tag" exact>{link.text}</NavLink>
             </li>
           ))}
         </ul>
       </div>
+
+      <div className="user-icon">
+        <FontAwesomeIcon icon={faUser} color="#0290ff" />
+      </div>
+
     </header>
   );
 };
